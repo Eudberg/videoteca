ab382a55ed5d9eac87857da2c69f4564f182ba64
Eudberg <42992509+Eudberg@users.noreply.github.com>
2023-05-21 16:12:55 -0300

    Add files via upload

---
+++ controllers/ControllerVideo.js
@@ -0,0 +1,44 @@
+const Video = require('../models/Video')
+module.exports = class VideoController{
+  static cadastrarVideo(req, res){
+    res.render('videos/cadastro');
+  }
+  static async VideoCreate(req,res){
+    const video = {
+    autor:req.body.autor,
+    titulo:req.body.titulo,
+    assunto:req.body.assunto,
+    descricao:req.body.descricao,
+    link:req.body.link
+    }
+    await Video.create(video)
+    res.send('Vídeo cadastrado')
+  }
+  static async listarVideo(req,res){
+    const video = await Video.findAll({raw:true});
+    res.render('videos/listar',{video});
+  }
+  static async UpdateVideo(req,res){
+    const id_video = req.params.id_video
+    const video = await Video.FindOne({where:{id_video:id_video},raw:true})
+      res.render("videos/update",{video})
+  }
+  static async VideoUpdate(req,res){
+    const id_video = req.body.id_video
+    const video = {
+      autor:req.body.autor,
+      titulo:req.body.titulo,
+      assunto:req.body.assunto,
+      descricao:req.body.descricao,
+      link:req.body.link
+    }
+    await Video.update(video, {where:{id_video:id_video}})
+    res.redirect("/")
+  }
+  static async excluir(req,res){
+    const id_video = req.body.id_video;
+    await Video.destroy({where:{id_video:id_video}})
+    res.redirect("/")
+  }
+  
+}
